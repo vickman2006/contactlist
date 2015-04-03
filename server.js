b3429bb4f3573d4febab54c8741053c1ace15062
Victor Vazquez <vickman2006@gmail.com>
2015-04-03 09:26:48 -0600

    test-inicio

---
+++ server.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
+
+
+var db = mongojs('contactlist',['contactlist']);
+
+/* Which mongo database and colleccion are we going to be using*/
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.get('/contactlist',function(req,res){
+	console.log("I received a GET request");
+
+	db.contactlist.find(function(err,docs){
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+
+app.post('/contactlist', function(req,res){
+	console.log(req.body);
+	db.contactlist.insert(req.body,function(req,doc){
+		res.json(doc);
+	});
+});
+
+app.delete('/contactlist/:id',function(req,res){
+	var id = req.params.id;
+	console.log("Contact id: " + id + " has been erased");
+	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err,doc){
+		res.json(doc);
+	});
+});
+
+app.get('/contactlist/:id', function(req,res){
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.findOne({_id: mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);
+	});
+});
+
+app.put('/contactlist/:id',function(req,res){
+	var id = req.params.id;
+	console.log(req.body.name);
+	db.contactlist.findAndModify(
+		{
+			query: {_id: mongojs.ObjectId(id)},
+			update: {$set: {name: req.body.name,
+							email: req.body.email,
+							number: req.body.number}},
+			new: true},
+			function(err,doc){
+				res.json(doc);
+			}
+	);
+});
+
+app.listen(3000);
+console.log("server running on port 3000");
+
+/* npm mongojs */
+/* npm body-parser */
